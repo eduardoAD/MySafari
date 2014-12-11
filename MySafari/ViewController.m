7061444751edad8d6b3ce42ecc19ac259bf5b656
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-12-10 18:08:23 -0600

    task #14 complete
    As a user, I want to see the current web site’s title in the navigation
    bar

--- MySafari/ViewController.m
+++ MySafari/ViewController.m
@@ -12,8 +12,9 @@
 @property (strong, nonatomic) IBOutlet UIWebView *webView;
 @property (strong, nonatomic) IBOutlet UITextField *urlTextField;
 @property (strong, nonatomic) IBOutlet UIButton *backButton;
-@property (strong, nonatomic) IBOutlet UIButton *stopButton;
+@property (strong, nonatomic) IBOutlet UIButton *statusButton;
 @property (strong, nonatomic) IBOutlet UIButton *forwardButton;
+@property (strong, nonatomic) IBOutlet UINavigationItem *navBar;
 
 @end
 
@@ -32,15 +33,8 @@
     if ([self.urlTextField.text isEqual:@""]) {
         return NO;
     } else {
-        NSString *urlText = self.urlTextField.text;
-        if (![urlText containsString:@"http://"]) {
-            urlText = [NSString stringWithFormat:@"http://%@",urlText];
-        }
-        NSURL *url = [[NSURL alloc] initWithString:urlText];
-        NSURLRequest *urlRequest = [[NSURLRequest alloc] initWithURL:url];
-        [self.webView loadRequest:urlRequest];
+        [self goButtonPressed];
         [textField resignFirstResponder];
-
         return YES;
     }
 }
@@ -48,12 +42,14 @@
 - (void)webViewDidStartLoad:(UIWebView *)webView {
     [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
     [self updateButtons];
-    self.urlTextField.text = webView.request.URL.absoluteString;
+
 }
 
 -(void)webViewDidFinishLoad:(UIWebView *)webView {
     [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
     [self updateButtons];
+    self.urlTextField.text = webView.request.URL.absoluteString;
+    self.navBar.title = [webView stringByEvaluatingJavaScriptFromString:@"document.title"];
 }
 
 - (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
@@ -74,7 +70,26 @@
     }
 }
 
-- (IBAction)onStopButtonPressed:(UIButton *)sender {
+- (IBAction)onStatusButtonPressed:(UIButton *)sender{
+    if ([self.webView isLoading]) {
+        [self stopButtonPressed];
+    }else{
+        [self goButtonPressed];
+    }
+}
+
+- (void)goButtonPressed {
+    NSString *urlText = self.urlTextField.text;
+    if (![urlText containsString:@"http://"]) {
+        urlText = [NSString stringWithFormat:@"http://%@",urlText];
+    }
+    NSURL *url = [[NSURL alloc] initWithString:urlText];
+    NSURLRequest *urlRequest = [[NSURLRequest alloc] initWithURL:url];
+    [self.webView loadRequest:urlRequest];
+
+}
+
+- (void)stopButtonPressed {
     [self.webView stopLoading];
 }
 
@@ -85,8 +100,24 @@
 
 - (void)updateButtons{
     self.forwardButton.enabled = self.webView.canGoForward;
+    if([self.forwardButton isEnabled]){
+        self.forwardButton.imageView.image = [UIImage imageNamed:@"next-24"];
+    }else{
+        self.forwardButton.imageView.image = [UIImage imageNamed:@"next-24-1"];
+    }
+
     self.backButton.enabled = self.webView.canGoBack;
-    self.stopButton.enabled = self.webView.loading;
+    if([self.backButton isEnabled]){
+        self.backButton.imageView.image = [UIImage imageNamed:@"previous-24"];
+    }else{
+        self.backButton.imageView.image = [UIImage imageNamed:@"previous-24-1"];
+    }
+
+    if ([self.webView isLoading]) {
+        self.statusButton.imageView.image = [UIImage imageNamed:@"close-25"];
+    }else{
+        self.statusButton.imageView.image = [UIImage imageNamed:@"redo-25"];
+    }
 }
 
 - (IBAction)onPlusButtonPressed:(UIButton *)sender {
@@ -97,8 +128,5 @@
     [alertView show];
 }
 
-- (IBAction)onClearButtonPressed:(UIButton *)sender {
-    self.urlTextField.text = @"";
-}
 
 @end
